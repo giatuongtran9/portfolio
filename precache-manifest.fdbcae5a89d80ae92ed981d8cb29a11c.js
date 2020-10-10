ca7448d43db003a8793fb08ed0275e74c5cfe533
unknown <trangiatuong777@gmail.com>
2020-10-09 20:57:07 -0400

    Updates

---
+++ precache-manifest.fdbcae5a89d80ae92ed981d8cb29a11c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9f6bd873612a342d80327476b840df",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0780ccd89e0a00f4a0b4",
+    "url": "/portfolio/static/css/main.e60e2510.chunk.css"
+  },
+  {
+    "revision": "7ef23d0d3bb69df22a13",
+    "url": "/portfolio/static/js/2.65ea1c0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.65ea1c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0780ccd89e0a00f4a0b4",
+    "url": "/portfolio/static/js/main.d484b79f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "07d3a8cc00a186ad0737649f533b0cff",
+    "url": "/portfolio/static/media/avatar.07d3a8cc.png"
+  },
+  {
+    "revision": "f636c6b7dbf5ef324a35971e13d7776a",
+    "url": "/portfolio/static/media/img-1.f636c6b7.png"
+  },
+  {
+    "revision": "cae732c86ebe748e3c3b4f0b77ce8708",
+    "url": "/portfolio/static/media/instagram.cae732c8.png"
+  }
+]);
