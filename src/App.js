a47de86c0b4f33a2ef6afa335dae790f1cb836b5
unknown <trangiatuong777@gmail.com>
2020-10-09 20:21:40 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Projects from './Projects/Projects'
+import About from './About/About'
+import Contact from './Contact/Contact'
+import Footer from './Footer/Footer'
+
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Header />
+      <Main />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
+    </HashRouter>
   );
 }
 
