e7e86317730e3841c0ea7add2470e1bb3f32e5a3
unknown <trangiatuong777@gmail.com>
2020-10-11 15:43:36 -0400

    add link

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => (
         <h1>Gia Tuong Tran</h1>
         <div className={footercss.icons}>
             <div className={footercss.icon}>
-                <a href="#facebook"><img src={facebook} alt={facebook} /></a>
+                <a href="https://www.facebook.com/profile.php?id=100005483011767" target="_blank" rel="noreferrer noopener"><img src={facebook} alt={facebook} /></a>
             </div>
             <div className={footercss.icon}>
-                <a href="#facebook"><img src={instagram} alt={instagram} /></a>
+                <a href="https://www.instagram.com/gttran.99/" target="_blank" rel="noreferrer noopener"><img src={instagram} alt={instagram} /></a>
             </div>
             <div className={footercss.icon}>
-                <a href="#facebook"><img src={linkedin} alt={linkedin} /></a>
+                <a href="https://www.linkedin.com/in/gia-tuong-tran-0a978017a/" target="_blank" rel="noreferrer noopener"><img src={linkedin} alt={linkedin} /></a>
             </div>
         </div>
     </section>
