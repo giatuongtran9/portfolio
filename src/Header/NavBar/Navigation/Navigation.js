a79e1d6ea412ed1fc33e72e47f5f3f3e6d8cb487
unknown <trangiatuong777@gmail.com>
2020-10-11 12:26:44 -0400

    hamburger not working properly

--- src/Header/NavBar/Navigation/Navigation.js
+++ src/Header/NavBar/Navigation/Navigation.js
@@ -3,16 +3,38 @@ import navcss from './Navigation.css'
 
 import { Link } from 'react-scroll'
 
-const Navigation = () => (
-    <div className={navcss.NavList}>
-        <ul>
-            <li><Link activeClass="active" to="main" spy={true} smooth={true} duration={1000} >Home</Link></li>
-            <li><Link activeClass="active" to="projects" spy={true} smooth={true} duration={1000}>Projects</Link></li>
-            <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={1000}>About</Link></li>
-            <li><Link activeClass="active" to="contact" spy={true} smooth={true} duration={1000}>Contact</Link></li>
-        </ul>
-    </div>
-)
+class Navigation extends React.Component {
+    state = {
+        isActive: false
+    }
+
+    clicked = () => {
+        this.setState({isActive: !this.state.isActive}, () => {
+            console.log("Called", this.state.isActive)
+        })
+    }
+
+    active = () => {
+        return (this.state.isActive===true) ? 'active' : null
+    }
+
+    render() {
+        return (
+        <div className={navcss.NavList}>
+            <div className={`${navcss.Hamburger} ${this.state.isActive ? 'active' : ''}`} onClick={this.clicked.bind(this)}>
+                <div 
+                    className={navcss.bar}>
+                </div>
+            </div>
+            <ul className={`${this.state.isActive ? 'active' : ''}`}>
+                <li><Link data-after="Home" activeClass="active" to="main" spy={true} smooth={true} duration={1000} >Home</Link></li>
+                <li><Link data-after="Projects" activeClass="active" to="projects" spy={true} smooth={true} duration={1000}>Projects</Link></li>
+                <li><Link data-after="About" activeClass="active" to="about" spy={true} smooth={true} duration={1000}>About</Link></li>
+                <li><Link data-after="Contact" activeClass="active" to="contact" spy={true} smooth={true} duration={1000}>Contact</Link></li>
+            </ul>
+        </div>
+        )}
+}
 
 export default Navigation
 
