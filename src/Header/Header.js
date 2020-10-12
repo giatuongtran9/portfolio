d0aeeff975ffff8f029bb3be27c367fac989c7d8
unknown <trangiatuong777@gmail.com>
2020-10-12 17:44:13 -0400

    minor adjust (need background for main component)

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,10 +3,44 @@ import headercss from './Header.css'
 
 import NavBar from './NavBar/NavBar'
 
-const Header = () => (
-    <section className={headercss.Header}>
-        <NavBar />
-    </section>
-)
+class Header extends React.Component {
+    state = {
+        a: {
+            backgroundColor: ''
+        }
+    }
+
+    scroll(component) {
+        const position = window.scrollY;
+        let background = this.state.a[component]
+        const q = {
+            ...this.state.a
+        };
+
+        if (position > 250) {
+            background = '#29323c'
+        } else {
+            background = 'transparent'
+        }
+        q[component] = background
+        this.setState({
+            a: q
+        })
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', () => this.scroll('backgroundColor'))
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', () => this.scroll('backgroundColor'))
+    }
+
+    render() {
+        return (
+        <section className={headercss.Header} style={this.state.a}>
+            <NavBar />
+        </section>
+)}}
 
 export default Header
