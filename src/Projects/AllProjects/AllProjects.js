154ef2c32b7a5f1e8cc23da03efc7c10a756e935
unknown <trangiatuong777@gmail.com>
2020-10-12 16:09:11 -0400

    minor adjust

--- src/Projects/AllProjects/AllProjects.js
+++ src/Projects/AllProjects/AllProjects.js
@@ -7,10 +7,10 @@ import Project from './Project/Project'
 const AllProjects = () => {
 
     const projectInfo = [
-        { name: 'Music', info: 'Describe about the app', image: image1 },
-        { name: 'Chat', info: 'Describe about the app', image: image1 },
-        { name: 'mindSpark', info: 'describe about the app', image: image1},
-        { name: 'Random', info: 'describe about the app', image: image1}
+        { name: 'Random', info: 'Full Stack Web Application using MERN stack (MongoDB, ExpressJS, ReactJS and NodeJS) to build an application where users can dynamically add, remove the ingredients of the burger and view the total money. The purpose of the application is to strengthen, improve ReactJS and learn more about Redux', image: image1},
+        { name: 'Music', info: 'Full Stack Web Application using ASP.NET and C# that allows users to add, edit, delete and view list of music and albums from a provided database and managed by SQL Server from and deploy to Microsoft Azure', image: image1 },
+        { name: 'SimpleChat', info: 'Web Application using NodeJS and SocketIO to build a simple real-time message for learning purpose', image: image1 },
+        { name: 'mindSpark', info: 'Full Stack Web Application using MERN stack (MongoDB, ExpressJS, ReactJS and NodeJS) to build a platform that people can upload and share with quotes that inspirit others. The website has all the basic function from create account, add, edit, delete, rate to manage personal information', image: image1},
     ]
 
     const project = projectInfo.map((p, index) => {
