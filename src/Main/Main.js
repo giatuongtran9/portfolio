c62952e37ee704ce7528d54147406a0bb86e2bec
unknown <trangiatuong777@gmail.com>
2020-10-13 20:52:42 -0400

    comment out the text

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,10 +5,10 @@ import { Link } from 'react-scroll'
 
 const Main = () => (
     <section id="main" className={maincss.Main}>
-        <h1>Hello, </h1>
+        {/* <h1>Hello, </h1>
         <h1> My Name is   </h1>
         <h1>Gia Tuong Tran </h1>
-        <Link activeClass="active" to="projects" spy={true} smooth={true} duration={1000}>Portfolio</Link>
+        <Link activeClass="active" to="projects" spy={true} smooth={true} duration={1000}>Portfolio</Link> */}
     </section>
 )
 
