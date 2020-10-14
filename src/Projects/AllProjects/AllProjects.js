1c012b0e4aff426297461aca972d40376996b5d7
unknown <trangiatuong777@gmail.com>
2020-10-13 20:52:51 -0400

    testing

--- src/Projects/AllProjects/AllProjects.js
+++ src/Projects/AllProjects/AllProjects.js
@@ -7,7 +7,7 @@ import Project from './Project/Project'
 const AllProjects = () => {
 
     const projectInfo = [
-        { name: 'Random', info: 'Full Stack Web Application using MERN stack (MongoDB, ExpressJS, ReactJS and NodeJS) to build an application where users can dynamically add, remove the ingredients of the burger and view the total money. The purpose of the application is to strengthen, improve ReactJS and learn more about Redux', image: image1},
+        { color: 'rgb(196, 210, 252)', name: 'Random', info: 'Full Stack Web Application using MERN stack (MongoDB, ExpressJS, ReactJS and NodeJS) to build an application where users can dynamically add, remove the ingredients of the burger and view the total money. The purpose of the application is to strengthen, improve ReactJS and learn more about Redux', image: image1},
         { name: 'Music', info: 'Full Stack Web Application using ASP.NET and C# that allows users to add, edit, delete and view list of music and albums from a provided database and managed by SQL Server from and deploy to Microsoft Azure', image: image1 },
         { name: 'SimpleChat', info: 'Web Application using NodeJS and SocketIO to build a simple real-time message for learning purpose', image: image1 },
         { name: 'mindSpark', info: 'Full Stack Web Application using MERN stack (MongoDB, ExpressJS, ReactJS and NodeJS) to build a platform that people can upload and share with quotes that inspirit others. The website has all the basic function from create account, add, edit, delete, rate to manage personal information', image: image1},
@@ -15,7 +15,7 @@ const AllProjects = () => {
 
     const project = projectInfo.map((p, index) => {
         return (
-            <Project index={index + 1} name = {p.name} info={p.info} image={p.image} />
+            <Project index={index + 1} name = {p.name} info={p.info} image={p.image} color = {p.color} />
         )
     })
 
