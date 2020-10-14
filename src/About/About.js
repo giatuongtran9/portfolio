06035616c323941b78b58831e1ba308e8d3772cc
unknown <trangiatuong777@gmail.com>
2020-10-13 20:51:22 -0400

    change color

--- src/About/About.js
+++ src/About/About.js
@@ -10,7 +10,7 @@ const About = () => (
             </div>
         </div>
         <div className={aboutcss.ColRight}>
-            <h1>About Me</h1>
+            <h1>About <span>Me</span></h1>
             <h2>Web Developer</h2>
             <p>A highly organized and passionate computer programming fresher with a strong understanding of the needs to deliver the best results in the role of Web Developer. My goad is to continually develop my programming skills in order to produce better solutions to the clients. I enjoy being challenged, working with projects outside my comfort zone and discovering new technologies and tools.</p>
             <a href="../download/GiaTuongTran.Resume.pdf" download>Download Resume</a>
