c4f7a9003955aa57fa42c907ca00968d979f7598
unknown <trangiatuong777@gmail.com>
2020-10-13 21:03:07 -0400

    image responsive mobile

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import maincss from './Main.css'
 
-import { Link } from 'react-scroll'
+import image from '../img/cover.png'
 
 const Main = () => (
     <section id="main" className={maincss.Main}>
+        <img src={image} alt={image} className={maincss.image} />
         {/* <h1>Hello, </h1>
         <h1> My Name is   </h1>
         <h1>Gia Tuong Tran </h1>
