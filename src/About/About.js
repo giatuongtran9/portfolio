ba07dcf742e3cf311d90d3e9bd45bed1647304ea
Gia Tuong Tran <trangiatuong999@gmail.com>
2021-09-12 20:25:16 -0400

    View pdf

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import aboutcss from './About.css'
 import Ava from '../img/avatar.png'
+import Resume from '../download/GiaTuongTran.Resume.pdf'
 
 const About = () => (
     <section id="about" className={aboutcss.About}>
@@ -13,7 +14,7 @@ const About = () => (
             <h1>About <span>Me</span></h1>
             <h2>Web Developer</h2>
             <p>A highly organized and passionate computer programming fresher with a strong understanding of the needs to deliver the best results in the role of Web Developer. My goad is to continually develop my programming skills in order to produce better solutions to the clients. I enjoy being challenged, working with projects outside my comfort zone and discovering new technologies and tools.</p>
-            <a href="../download/GiaTuongTran.Resume.pdf" download>Download Resume</a>
+            <a href={Resume} target="_blank">View here</a>
         </div>
     </section>
 )
